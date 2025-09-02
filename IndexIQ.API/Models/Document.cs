718ce71026d311e21ccc8a380ce6114628486b25
adedamola-toye <toye.adedamola@gmail.com>
2025-09-02 13:45:18 +0100

    add Document model to Models namespace

--- IndexIQ.API/Models/Document.cs
+++ IndexIQ.API/Models/Document.cs
@@ -1,7 +1,11 @@
-public class Document{
-    public  string id;
-    public string FileName {get; set;};
-    public string ContentText {get; set;};
-    public Dictionary<string, int> TermFrequency {get; set;};
-    public DateTime UploadedTimestamp {get; set;};
+namespace IndexIQ.API.Models
+{
+    public class Document
+{
+    public required string id;
+    public required string FileName { get; set; }
+    public required string ContentText { get; set; }
+    public required Dictionary<string, int> TermFrequency { get; set; }
+    public DateTime UploadedTimestamp { get; set; }
+}
 }
